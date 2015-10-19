77b0b1537ff6cbc73457a72d45ff85bea6ecbabd
Paolo Brandoli <paolo.brandoli@cosylab.com>
2015-10-19 22:31:23 +0200

    Added test for allocation of two devices at the same time, PV subscription

--- include/nds3impl/logStreamGetterImpl.h
+++ include/nds3impl/logStreamGetterImpl.h
@@ -9,11 +9,22 @@ namespace nds
 
 class BaseImpl;
 
+
 class NDS3_API LogStreamGetterImpl
 {
 public:
     virtual ~LogStreamGetterImpl();
 
+    /**
+     * @brief Allocates a new stream logger and returns it.
+     *
+     * It is the responsability of the caller to delete the logger when it is no longer
+     *  needed.
+     *
+     * @param node     the node that will stream data to the new log stream
+     * @param logLevel the log level
+     * @return         the newly allocate log stream
+     */
     virtual std::ostream* getLogStream(const BaseImpl& node, const logLevel_t logLevel) = 0;
 };
 }
